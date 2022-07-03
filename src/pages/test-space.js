a566fe70907336d4ce50fb27d3fa17501f9a3c9e
dwroyal02 <dwroyal02@gmail.com>
2022-07-03 10:04:43 +0900

    Head 컴포넌트 도입

--- src/pages/test-space.js
+++ src/pages/test-space.js
@@ -1,5 +1,6 @@
-import { Link } from 'gatsby';
+
 import * as React from "react"
+import Head from '../components/head';
 
 // styles
 
@@ -8,12 +9,7 @@ const TestSpacePage= () => {
   return (
     <main>
       <title>테스트스페이스</title>
-      <div style={{display:"flex", gap: 10}}>
-        <Link to="/">홈</Link>
-        <Link to="/about">어바웃</Link>
-        <Link to="/test">테스트</Link>
-        <Link to="/test-space">테스트-스페이스</Link>
-      </div>
+      <Head />
       <h1>테스트 스페이스</h1>
     </main>
   )
--- src/pages/test.js
+++ src/pages/test.js
@@ -1,5 +1,5 @@
-import { Link } from 'gatsby';
 import * as React from "react"
+import Head from '../components/head';
 
 // styles
 
@@ -8,12 +8,7 @@ const TestPage= () => {
   return (
     <main>
       <title>테스트</title>
-      <div style={{display:"flex", gap: 10}}>
-        <Link to="/">홈</Link>
-        <Link to="/about">어바웃</Link>
-        <Link to="/test">테스트</Link>
-        <Link to="/test-space">테스트-스페이스</Link>
-      </div>
+      <Head />
       <h1>테스트</h1>
     </main>
   )
