10dcb363c8201ad2ca958d1610cd857568e0b97a
dwroyal02 <dwroyal02@gmail.com>
2022-07-03 09:49:30 +0900

    GNB 추가

--- src/pages/test.js
+++ src/pages/test.js
@@ -4,19 +4,19 @@ import * as React from "react"
 // styles
 
 // markup
-const IndexPage = () => {
+const TestPage= () => {
   return (
     <main>
-      <title>Home Page!!!</title>
+      <title>테스트</title>
       <div style={{display:"flex", gap: 10}}>
         <Link to="/">홈</Link>
         <Link to="/about">어바웃</Link>
         <Link to="/test">테스트</Link>
         <Link to="/test-space">테스트-스페이스</Link>
       </div>
-      <h1>Home Page</h1>
+      <h1>테스트</h1>
     </main>
   )
 }
 
-export default IndexPage;
+export default TestPage;
